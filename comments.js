f85a5a32658f228e814ffd05fac3428dab52af48
Johnnik001 <82582264+Johnnik001@users.noreply.github.com>
2023-07-26 09:22:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,54 +1,22 @@
-// create web browser
-const browser = await puppeteer.launch({
-    headless: false
-});
-// create new page
-const page = await browser.newPage();
-// go to url
-await page.goto('https://www.instagram.com/accounts/login/');
-// wait for element defined by XPath appear in page
-await page.waitForXPath('//input[@name="username"]');
-// type account
-await page.type('input[name="username"]', 'your_username');
-// type password
-await page.type('input[name="password"]', 'your_password');
-// click button "Log In"
-await page.click('button[type="submit"]');
-// wait for navigation to finish
-await page.waitForNavigation();
-// click button "Not Now"
-await page.click('button[type="button"]');
-// wait for element defined by XPath appear in page
-await page.waitForXPath('//a[@href="/explore/"]');
-// go to url
-await page.goto('https://www.instagram.com/explore/');
-// wait for element defined by XPath appear in page
-await page.waitForXPath('//div[@class="v1Nh3 kIKUG  _bz0w"]');
-// get all elements defined by XPath
-const [all] = await page.$x('//div[@class="v1Nh3 kIKUG  _bz0w"]');
-// click element
-await all.click();
-// wait for navigation to finish
-await page.waitForNavigation();
-// wait for element defined by XPath appear in page
-await page.waitForXPath('//button[@class="sqdOP yWX7d    y3zKF     "]');
-// get all elements defined by XPath
-const [like] = await page.$x('//button[@class="sqdOP yWX7d    y3zKF     "]');
-// click element
-await like.click();
-// wait for navigation to finish
-await page.waitForNavigation();
-// wait for element defined by XPath appear in page
-await page.waitForXPath('//textarea[@class="Ypffh"]');
-// get all elements defined by XPath
-const [comment] = await page.$x('//textarea[@class="Ypffh"]');
-// type comment
-await comment.type('This is my comment');
-// wait for element defined by XPath appear in page
-await page.waitForXPath('//button[@type="submit"]');
-// get all elements defined by XPath
-const [submit] = await page.$x('//button[@type="submit"]');
-// click element
-await submit.click();
-// wait for navigation to finish
-await page.waitForNavigation();
+// create new browser window
+const { BrowserWindow } = require('electron').remote
+const path = require('path')
+const url = require('url')
+const ipc = require('electron').ipcRenderer
+
+const submitBtn = document.getElementById('submitBtn')
+const commentList = document.getElementById('commentList')
+const commentField = document.getElementById('commentField')
+
+submitBtn.addEventListener('click', function (event) {
+  const comment = commentField.value
+  commentField.value = ''
+  ipc.send('addComment', comment)
+})
+
+ipc.on('newComment', function (event, arg) {
+  const li = document.createElement('li')
+  const text = document.createTextNode(arg)
+  li.appendChild(text)
+  commentList.appendChild(li)
+})
